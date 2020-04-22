90ea1ec38ef45263a88d6c3afe60ddb659df4042
nejmi <uyghur@nejmis-MacBook-Pro.local>
2020-04-21 22:34:55 -0500

    second commit

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,15 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hello world");
+        System.out.println("hello Najmidin");
+        System.out.println("i am working ");
+
+    }
+
+
+
+
+}
---
+++ src/test/java/secondClass.java
@@ -0,0 +1,12 @@
+public class secondClass {
+
+    public static void main(String[] args) {
+
+        String name = "Raziye....";
+
+        System.out.println(name);
+
+
+
+    }
+}
